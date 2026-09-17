4c879c78517f638a7cb43cdaa2b34e86bf325608
SIR FLOREX 🪄 <hereflorex@gmail.com>
2026-09-17 12:24:33 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+
 from app import create_app
 from app.extensions import socketio
 
@@ -8,5 +9,6 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+        port=int(os.environ.get("PORT", 5000)),
+        allow_unsafe_werkzeug=True
     )
